faea2a3fff64096b17be7966ea13ab6d7f487804
mimcmahon20 <mimcmahon20@vt.edu>
2023-04-01 22:06:25 -0400

    Work & Project parallax
    -added another layer moving in opposite direction and at different speed.

--- resources/js/projectanimation.js
+++
@@ -1,6 +0,0 @@
-let background = document.querySelector('.innerbackground');
-let y = background.getBoundingClientRect().top;
-
-window.addEventListener('scroll', function() {
-    background.style.backgroundPositionX = y + window.pageYOffset + 'px';
-});
