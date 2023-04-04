e8bcb413527228ce73eef90c8f487c1b5f7195f7
mimcmahon20 <mimcmahon20@vt.edu>
2023-04-04 19:59:35 -0400

    Scroll, Snake & Splash
    -Removed entire splash section/version
    -Removed background of legend section
    -Added snake script as the background of the legend
    -Made it appear to be sticky while everything scrolls over it.

---
+++ resources/js/dj.js
@@ -0,0 +1,95 @@
+import { rows, totalRows, totalCols } from "./snakescript.js";
+
+export function dijkstra(startX, startY, goalX, goalY, snakey) {
+      // Initialize the distances and paths to all nodes
+  let distances = [];
+  let paths = [];
+  for (let i = 0; i < totalRows + totalCols; i++) {
+    distances[i] = new Array(totalRows).fill(Infinity);
+    paths[i] = new Array(totalRows).fill(null);
+  }
+  distances[startX][startY] = 0;
+
+
+  // Create a set of unvisited nodes
+let unvisited = new Set();
+for (let i = 0; i < totalRows; i++) {
+  for (let j = 0; j < totalCols; j++) {
+    let skipNode = false;
+    //checks to see if the node is in the snake's body
+    for (let segment of snakey.body.slice(0, -1)) {
+      if (rows[i].boxes[j] === segment) {
+        skipNode = true;
+        break;
+      }
+    }
+    //if it is not in the snake's body, add it to the unvisited set
+    if (!skipNode) {
+      unvisited.add([j, i]);
+    }
+  }
+}
+
+
+  // Loop until we visit all nodes
+  while (unvisited.size > 0) {
+    
+    // Find the node with the shortest distance from the start node
+    let minDistance = Infinity;
+    let minNode;
+    for (let node of unvisited) {
+      if (distances[node[0]][node[1]] < minDistance) {
+        minDistance = distances[node[0]][node[1]];
+        minNode = node;
+      }
+    }
+    // Remove the node from the unvisited set
+    unvisited.delete(minNode);
+    if(minNode == null) {
+      return null;
+    } else {
+      // If we have reached the end node, return the path
+      if (minNode[0] === goalX && minNode[1] === goalY) {
+        let path = [];
+        let currentNode = minNode;
+        while (currentNode !== null) {
+          path.unshift(currentNode);
+          currentNode = paths[currentNode[0]][currentNode[1]];
+          
+        }
+        return path;
+      }
+
+      // Update the distances and paths of the neighboring nodes
+      let neighbors = getNeighbors(minNode[0], minNode[1], snakey);
+      for (let neighbor of neighbors) {
+        let distance = distances[minNode[0]][minNode[1]] + 1;
+        if (distance < distances[neighbor[0]][neighbor[1]]) {
+          distances[neighbor[0]][neighbor[1]] = distance;
+          paths[neighbor[0]][neighbor[1]] = minNode;
+        }
+      }
+    }
+  }
+
+  // If we haven't found a path to the end node, return null
+  console.log("No path found");
+  return null;
+
+  function getNeighbors(x, y, snakey) {
+    let neighbors = [];
+    if( x > 0 && !rows[y].boxes[x-1].box.classList.contains("wall")) {
+      neighbors.push([x-1, y]);
+    }
+    if( x < totalCols-1 && !rows[y].boxes[x+1].box.classList.contains("wall")) {
+      neighbors.push([x+1, y]);
+    }
+    if( y > 0 && !rows[y-1].boxes[x].box.classList.contains("wall")) {
+      neighbors.push([x, y-1]);
+    }
+    if( y < totalRows-1 && !rows[y+1].boxes[x].box.classList.contains("wall")) {
+      neighbors.push([x, y+1]);
+    }
+    return neighbors;
+  }
+}
---
+++ resources/js/snakescript.js
@@ -0,0 +1,276 @@
+import { dijkstra } from "./dj.js";
+
+let game = document.getElementsByClassName("game")[0];
+let dimension = getComputedStyle(document.documentElement).getPropertyValue('--snakesize');
+dimension = parseInt(dimension);
+console.log(dimension);
+let viewportWidth = window.innerWidth;
+let viewportHeight = window.innerHeight - 60;
+
+console.log(`Viewport width: ${viewportWidth}`);
+console.log(`Viewport height: ${viewportHeight}`);
+
+let timer = 42;
+
+//setting up the grid
+class box {
+    constructor() {
+        this.box = document.createElement("div");
+        this.box.classList.add("box");
+    }
+}
+
+// This class creates a row of boxes, and defines a function that adds a box to the row.
+
+
+class row {
+    constructor() {
+        this.row = document.createElement("div");
+        this.row.classList.add("row");
+        game.appendChild(this.row); 
+        this.boxes = new Array();
+    }
+
+    addBox() {
+        let boxy = new box();
+        this.boxes.push(boxy);
+        this.row.appendChild(boxy.box);
+    }
+}
+// create an array to store all the rows of boxes
+export let totalRows = Math.floor(viewportHeight/(dimension+2));
+export let totalCols = Math.floor(viewportWidth/(dimension+2));
+export let rows = new Array();
+// loop through the total number of rows
+for(let i = 0; i < totalRows; i++) { //i is row number or y
+    // create a new row object and store it in the rows array
+    rows[i] = new row();
+    // loop through the total number of boxes in each row
+    for(let j = 0; j < totalCols; j++) { //j is column number or x
+        // add a box to the row
+        rows[i].addBox();
+        // if the box is on the edge of the grid, add the wall class
+        if(i == 0|| j == 0 || i == totalRows-1 || j == totalCols-1) {
+            rows[i].boxes[j].box.classList.add("wall");
+        }
+    }
+}
+
+class food {
+    constructor() {
+        // The food will be placed anywhere in the grid but not on the borders
+        this.x = Math.floor(Math.random() * (totalCols-1)+1);
+        this.y = Math.floor(Math.random() * (totalRows-1)+1);
+        // If the food is placed on the border or on the snake, it will be placed again
+        while(this.x == 0 || this.y == 0 || this.x == totalCols-1 || this.y == totalRows-1
+            || rows[this.y].boxes[this.x].box.classList.contains("snake")) { 
+            this.x = Math.floor(Math.random() * (totalCols-1)+1);
+            this.y = Math.floor(Math.random() * (totalRows-1)+1);
+        }
+        this.box = rows[this.y].boxes[this.x];
+        this.box.box.classList.add("food");
+    }
+}
+
+//the main snake class
+class snake {
+    constructor() {
+        this.alive = true;
+
+        //body
+        this.body = new Array();
+        //tail
+        this.tail = rows[1].boxes[1];
+        this.tail.box.classList.add("snake");
+        this.body.push(this.tail);
+        //head
+        this.head = rows[1].boxes[2];
+        this.head.box.classList.add("snake");
+        this.head.box.classList.add("snake-head");
+        this.body.push(this.head);
+        this.x = 2;
+        this.y = 1;
+        //direction
+        this.direction = "right";
+
+        //direction queue
+        this.queue = new Array();
+    }
+
+    next() {
+        if(this.direction == "right") {
+            return rows[this.y].boxes[this.x+1];
+        }
+        if(this.direction == "left") {
+            return rows[this.y].boxes[this.x-1];
+        }
+        if(this.direction == "up") {
+            return rows[this.y-1].boxes[this.x];
+        }
+        if(this.direction == "down") {
+            return rows[this.y+1].boxes[this.x];
+        }
+    }
+
+    move() {
+        this.head.box.classList.remove("snake-head");
+        checkCollision(this, this.next());
+        this.head = this.next();
+        if(this.direction == "right") {
+            this.x++;
+        }
+        if(this.direction == "left") {
+            this.x--;
+        }
+        if(this.direction == "up") {
+            this.y--;
+        }
+        if(this.direction == "down") {
+            this.y++;
+        }
+        this.head.box.classList.add("snake-head");
+        this.head.box.classList.add("snake");
+        this.body[0].box.classList.remove("snake");
+        this.body.shift();
+        this.body.push(this.head);
+    }
+
+    eat() {
+        this.head.box.classList.remove("snake-head");
+        checkCollision(this, this.next());
+        if(this.direction == "right") {
+            this.head = rows[this.y].boxes[this.x+1];
+            this.x++;
+        }
+        if(this.direction == "left") {
+            this.head = rows[this.y].boxes[this.x-1];
+            this.x--;
+        }
+        if(this.direction == "up") {
+            this.head = rows[this.y-1].boxes[this.x];
+            this.y--;
+        }
+        if(this.direction == "down") {
+            this.head = rows[this.y+1].boxes[this.x];
+            this.y++;
+        }
+        
+        this.head.box.classList.add("snake-head");
+        this.head.box.classList.add("snake");
+        this.head.box.classList.remove("food");
+        this.body.push(this.head);
+        this.tail = this.body[0];
+    }
+
+}
+
+function checkCollision(snakey, box) {
+    if(box == null){
+        snakey.alive = false;
+        console.log("game over null");
+    }
+    if(box.box.classList.contains("wall") || box.box.classList.contains("snake")) {
+        snakey.alive = false;
+        console.log("game over");
+    }
+}
+
+// document.addEventListener("keydown", function(event) { 
+//     if(event.key == "ArrowRight") {
+//         snakey.direction = "right";
+//     }
+//     if(event.key == "ArrowLeft") {
+//         snakey.direction = "left";
+//     }
+//     if(event.key == "ArrowUp") {
+//         snakey.direction = "up";
+//     }
+//     if(event.key == "ArrowDown") {
+//         snakey.direction = "down";
+//     }
+// });
+
+let snakey = new snake();
+let foody = new food();
+
+
+let interval = setInterval(runner, timer, snakey);
+
+let intervalClearer = setInterval(function() {
+    if(!snakey.alive) {
+        clearInterval(interval);
+        for(let i = 0; i < rows.length; i++) {
+            for(let j = 0; j < rows[i].boxes.length; j++) {
+                rows[i].boxes[j].box.classList.remove("snake", "snake-head", "food");
+            }
+        }
+        snakey = new snake();
+        foody = new food();
+        console.log('reset');
+        interval = setInterval(runner, timer, snakey);
+    }}
+,25);
+
+intervalClearer;
+
+//MAIN FUNCTION
+function runner(snakey) {
+    //Update the queue of the snake
+    snakey.queue = queueDJ(snakey);
+    //removing the null entry
+    snakey.queue.shift();
+    //checks to see if next tile is food OR if the snake's head is already on food somehow
+    if(snakey.next() == foody.box || snakey.head.box.classList.contains("food")) {
+        snakey.queue = new Array();
+        console.log("food eaten");
+        foody.box.box.classList.remove("food");
+        snakey.eat();//replaces food with head, rest of snake does not move
+        foody = new food(); //spawns in new piece of food at random location not on snake
+    } else {
+        snakey.direction = snakey.queue[0];
+        //if the snake is not on food, it moves normally
+        if(snakey.next() == foody.box) {
+            snakey.eat();
+            snakey.queue = new Array();
+            snakey.queue = queueDJ(snakey);
+            console.log("food eaten specially");
+            foody.box.box.classList.remove("food");
+            foody = new food();
+        } else {
+            snakey.direction = snakey.queue.shift();
+            snakey.move();
+        }
+    }
+    
+}
+
+function queueDJ(snakey) {
+    let dj = dijkstra(snakey.x, snakey.y, foody.x, foody.y, snakey);
+    let currentX = snakey.x;
+    let currentY = snakey.y;
+    if(dj === null) {
+        console.log("bug");
+        snakey.queue.push("right");
+        return snakey.queue;
+    }
+    
+    for(let i = 0; i < dj.length; i++) {
+        if(dj[i][0] == currentX-1){
+            snakey.queue[i] = "left";
+            currentX--;
+        }
+        if(dj[i][0] == currentX+1){
+            snakey.queue[i] = "right";
+            currentX++;
+        }
+        if(dj[i][1] == currentY-1){
+            snakey.queue[i] = "up";
+            currentY--;
+        }
+        if(dj[i][1] == currentY+1){
+            snakey.queue[i] = "down";
+            currentY++;
+        }
+    }
+    return snakey.queue;
+}
