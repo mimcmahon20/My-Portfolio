eafa81a30e78eb2f10cc6cfd8b94cf95f9ae42cd
mimcmahon20 <mimcmahon20@vt.edu>
2023-04-11 00:16:39 -0400

    Colors!
    -finally removing the monochrome look from the website, consider seeing this deployment if implementing a dark/light mode and or a colorblind mode

--- resources/js/projectcards.js
+++ resources/js/projectcards.js
@@ -1,15 +1,20 @@
-let cardContainer = document.querySelectorAll('.card-container');
-let project = document.querySelectorAll('.project');
-let rotate = document.querySelectorAll('.rotate');
-
-
-rotate.forEach((rotate, i) => {
-    rotate.addEventListener('click', () => {
-        console.log('click');
-        if(i%2 != 0) {
-            project[Math.floor(i/2)].style.transform = 'rotateY(0deg)';
-        } else {
-            project[Math.floor(i/2)].style.transform = 'rotateY(180deg)';
-        }
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('mouseenter', e => {
+      const line = link.querySelector('::after');
+      line.style.width = '0'; /* Reset the width to 0 */
+      line.style.display = 'block'; /* Show the line */
     });
-});
+    
+    link.addEventListener('mousemove', e => {
+      const line = link.querySelector('::after');
+      const rect = link.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left;
+      line.style.width = mouseX + 'px'; /* Update the width based on mouse position */
+      line.style.left = '0'; /* Reset the left position */
+    });
+    
+    link.addEventListener('mouseleave', e => {
+      const line = link.querySelector('::after');
+      line.style.opacity = '0'; /* Hide the line */
+    });
+  });
